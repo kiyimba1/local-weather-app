fddb3134f8fae8b2b2af9bcd8ffc659b17013418
kiyimba1 <edwineddie54@gmail.com>
2022-03-07 01:31:35 +0300

    Added content to current weather cxomponent

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ICurrentWeather } from '../interface';
+
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
   styleUrls: ['./current-weather.component.css']
 })
 export class CurrentWeatherComponent implements OnInit {
+  current: ICurrentWeather
 
-  constructor() { }
+  constructor() {
+    this.current = {
+      city: 'Kampala',
+      country: 'UG',
+      date: new Date(),
+      image: 'assets/img/sunny.png',
+      temperature: 72,
+      description: 'Sunny'
+    } as ICurrentWeather
+  }
 
   ngOnInit(): void {
   }
