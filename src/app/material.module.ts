5bd3287ce7a000b91aec2bfbff0fdcd8aa11f3d8
kiyimba1 <edwineddie54@gmail.com>
2022-03-09 19:10:24 +0300

    Implemented mat Card

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
 import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatCardModule } from '@angular/material/card'
 
 
-const modules = [MatButtonModule, MatIconModule, MatToolbarModule]
+const modules = [MatButtonModule, MatIconModule, MatToolbarModule, MatCardModule]
 
 @NgModule({
   declarations: [],
