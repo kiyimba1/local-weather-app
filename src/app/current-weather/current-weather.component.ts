c87cbb422450d8e5b2f1f14d199ce610407d07da
kiyimba1 <edwineddie54@gmail.com>
2022-03-10 00:11:25 +0300

    Customized Material

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -18,4 +18,9 @@ export class CurrentWeatherComponent implements OnInit {
     this.weatherService.getCurrentWeather('Kampala', 'UG').subscribe((data) => this.current = data)
   }
 
+  getOrdinal(date: number) {
+    const n = new Date(date).getDate()
+    return n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : ''
+  }
+
 }
