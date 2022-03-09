8ed6cde6c2e7a047039e30503c405ccaa0055af1
kiyimba1 <edwineddie54@gmail.com>
2022-03-09 18:09:22 +0300

    Imported common material modules

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 
+const modules = [MatButtonModule, MatIconModule, MatToolbarModule]
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: modules,
+  exports: modules
 })
 export class MaterialModule { }
